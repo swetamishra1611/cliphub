ecb4c150a1eda4d14ef54a4576d023afcf8b3bb0
Sweta Kumari <112224398+swetamishra1611@users.noreply.github.com>
2025-09-16 11:11:05 +0000

    twentymin

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import { NextAuthOptions} from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+
