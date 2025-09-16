34e560b2cef414ae1d02d213c9576fc726db46d1
Sweta Kumari <112224398+swetamishra1611@users.noreply.github.com>
2025-09-16 20:22:15 +0000

    thirtyfive

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,56 @@
-import { NextAuthOptions} from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs"; // import bcrypt
+import { connectToDatabase } from "./db";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials:{
+        email:{label:"Email",type: "text"},
+        password: {label: "Password",type:"password"}
+      },
+      async authorize(credentials){
+        if(!credentials?.email || !credentials?.password){
+          throw new Error ("Missing email or password")
+        }
+
+        try{
+          await connectToDatabase()
+          const user = await User.findOne({email: credentials.email});
+
+          if(!user){
+            throw new Error("No user found with this");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          )
+
+          if(!isValid){
+            throw new Error("invalid password");
+          }
+
+          return{
+            id: user._id.toString(),
+            email:user.email
+          }
+        }catch(error){
+          console.error("Auth error",error)
+          throw error
+
+        }
+      },
+
+
     }),
+
     // ...add more providers here
   ],
+
+  
 };
 
